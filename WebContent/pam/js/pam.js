b21019bfbfcbb795a9aa76b8d298703512110c03
HQMIS <qiminis0801@gamil.com>
2013-07-04 01:42:58 +0800

    rename all the funtion's paras in pam.js in order to easy to distinguish

--- WebContent/pam/js/pam.js
+++ WebContent/pam/js/pam.js
@@ -25,13 +25,14 @@ var picList = [ url + "tyr.jpg", url + "xm.jpg", url + "jyf.jpg",
  * @description control to broadcast audio
  * 
  * @param {int}
- *            mode 0 - diy resource(Array) 1 - use server's resource(String)
- * @param {Array
- *            or Int} res Array - path of diy resource Int - broadcast mode, 0
- *            for single replay, 1 for more replay
+ *            _audio_mode 0 - diy resource(Array) 1 - use server's resource(String)
+ * @param {Array or Int} 
+ * 			  _audio_res 
+ *            Array - path of diy resource 
+ *            Int - broadcast _audio_mode, 0 for single replay, 1 for more replay
  * @return {null} null
  */
-function _audio(mode, res) {
+function _audio(_audio_mode, _audio_res) {
 	/* check what browser you are using */
 	var Sys = {};
 	var ua = navigator.userAgent.toLowerCase();
@@ -43,10 +44,10 @@ function _audio(mode, res) {
 			.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
 
 	/**
-	 * when mode equals 1, we should use server's audio else when mode equals 0,
+	 * when _audio_mode equals 1, we should use server's audio else when _audio_mode equals 0,
 	 * we should use diy audio
 	 */
-	if (1 == mode) {
+	if (1 == _audio_mode) {
 		/* audio list on server */
 		// var audioList = ["pam/audio/snq", "pam/audio/qg", "pam/audio/xy"]; /*
 		// the final goal is get the list from server */
@@ -56,7 +57,7 @@ function _audio(mode, res) {
 		if (Sys.ie) {
 			_audio_IE(audioList);
 		} else {
-			if (1 == res) { // order play the audio on server
+			if (1 == _audio_res) { // order play the audio on server
 				broadcastList(audioList);
 			} else { // order play the audio on server
 				var slideIndex = parseInt(Math.random() * audioList.length);
@@ -70,14 +71,14 @@ function _audio(mode, res) {
 		 * tag
 		 */
 		if (Sys.ie) {
-			_audio_IE(res);
+			_audio_IE(_audio_res);
 		} else {
-			broadcastList(res);
+			broadcastList(_audio_res);
 		}
 	}
 }
 
-function _audio_IE(res){
+function _audio_IE(_audio_res) {
 	/* create div && append to body */
 	var _notice = document.createElement("div");
 	var object = document.body.appendChild(_notice);
@@ -97,7 +98,7 @@ function _audio_IE(res){
 			_bgsound.src = "";
 			_switch.innerText = "打开背景音乐";
 		} else {
-			_bgsound.src = res[0] + ".mp3";
+			_bgsound.src = _audio_res[0] + ".mp3";
 			_switch.innerText = "关闭背景音乐";
 		}
 	};
@@ -107,7 +108,7 @@ function _audio_IE(res){
 	/* create bgsound && set id\src\loop && append to body */
 	var _bgsound = document.createElement("bgsound");
 	_bgsound.id = "_bgsound";
-	_bgsound.src = res[0] + ".mp3";
+	_bgsound.src = _audio_res[0] + ".mp3";
 	_bgsound.loop = -1;
 	var object = document.body.appendChild(_bgsound);
 }
@@ -116,10 +117,10 @@ function _audio_IE(res){
  * @description broadcast audio by list
  * 
  * @param {String}
- *            res the audio list to be broadcast
+ *            _audio_res the audio list to be broadcast
  * @return {null} null
  */
-function broadcastList(res) {
+function broadcastList(_audio_res) {
 	/* audioIndex is the index of resource array */
 	var audioIndex = 0;
 	/* create audio */
@@ -133,18 +134,18 @@ function broadcastList(res) {
 		/* append audio to body */
 		var object = document.body.appendChild(_audio);
 
-		/* broadcat res[i] through audio */
-		broadcast(_audio, res[audioIndex++]);
+		/* broadcat _audio_res[i] through audio */
+		broadcast(_audio, _audio_res[audioIndex++]);
 
 		/* add the callback function addEventListener to audio's object */
 		_audio.addEventListener('ended', function() {
 			/* judge the index of the audio to be broadcast */
-			if (audioIndex >= res.length) {
+			if (audioIndex >= _audio_res.length) {
 				audioIndex = 0;
 			}
 
-			/* broadcat res[i] through audio */
-			broadcast(_audio, res[audioIndex++]);
+			/* broadcat _audio_res[i] through audio */
+			broadcast(_audio, _audio_res[audioIndex++]);
 		}, false);
 	} else {
 		var _notice = document.createElement("div");
@@ -159,10 +160,10 @@ function broadcastList(res) {
  * @param {object}
  *            _audio the object create by document.createElement("audio")
  * @param {String}
- *            res the audio to be broadcast
+ *            _audio_res the audio to be broadcast
  * @return {null} null
  */
-function broadcast(_audio, res) {
+function broadcast(_audio, _audio_res) {
 	/* set id\controls\preload\autoplay\className */
 	_audio.id = "_audio";
 	_audio.controls = "controls";
@@ -175,13 +176,11 @@ function broadcast(_audio, res) {
 	 * or not && select the suitable format's audio to be broadcast
 	 */
 	if (_audio.canPlayType("audio/ogg")) {
-		_audio.src = res + ".ogg";
+		_audio.src = _audio_res + ".ogg";
 	} else if (_audio.canPlayType("audio/mpeg")) {
-		_audio.src = res + ".mp3";
+		_audio.src = _audio_res + ".mp3";
 	}
 }
-
-
 
 /**
  * ########## ########## ########## ########## ########## ########## ########## ########## ########## *
@@ -192,80 +191,81 @@ function broadcast(_audio, res) {
  */
 
 /**
- * @description control to set background
+ * @description control to set background use picture
  * 
  * @param {int}
- *            mode 0 - diy resource(Array) 1 - use server's resource(String)
+ *            _bg_mode 0 - diy resource(Array) 1 - use server's resource(String)
  * @param {Array
- *            or Int} res Array - path of diy resource Int - show background
- *            mode, 0 for just one image, 1 for slide image
+ *            or Int} _bg_res Array - path of diy resource Int - show background
+ *            _bg_mode, 0 for just one image, 1 for slide image
  * @param {int}
- *            time the interval time of the timer, unit is millisecond(毫秒)
+ *            _bg_time the interval time of the timer, unit is millisecond(毫秒)
  * @return {null} null
  */
-function _bg(mode, res, time) {
+function _bg(_bg_mode, _bg_res, _bg_time) {
 	/**
-	 * when mode equals 1, we should use server's picture else when mode equals
+	 * when _bg_mode equals 1, we should use server's picture else when _bg_mode equals
 	 * 0, we should use diy picture
 	 */
-	if (1 == mode) {
-		if (1 == res) { // order to set picture on server in the list as
+	if (1 == _bg_mode) {
+		if (1 == _bg_res) { // order to set picture on server in the list as
 			// background
 			/* set a timer, to set background every interval time */
-			var timeId = window
-					.setInterval("slideSetBackground(picList)", time);
+			var timeId = window.setInterval("slideSetBackground(picList)",
+					_bg_time);
 			slideSetBackground(picList);
 		} else { // set one picture as background
 			var slideIndex = parseInt(Math.random() * picList.length);
 			setBackground(picList.slice(slideIndex, slideIndex + 1));
 		}
 	} else {
-		if (1 == res.length) {
+		if (1 == _bg_res.length) {
 			/*
 			 * the length of the array is 1, direct set the picture index which
 			 * index is 0 as background
 			 */
-			setBackground(res[0]);
+			setBackground(_bg_res[0]);
 		} else {
 			/* set a timer, to set background every interval time */
-			var timeId = window.setInterval("slideSetBackground(res)", time);
-			slideSetBackground(res);
+			var timeId = window.setInterval("slideSetBackground(_bg_res)",
+					_bg_time);
+			slideSetBackground(_bg_res);
 		}
 	}
 }
 
 /**
- * @description slide to set background
+ * @description slide to set background use picture
  * 
  * @param {Array}
- *            res the picture list to be set as background
+ *            _bg_res the picture list to be set as background
  * @return {null} null
  */
-function slideSetBackground(res) {
+function slideSetBackground(_bg_res) {
 	// alert(imgIndex);
-	var bgImg = res[imgIndex++];
+	var bgImg = _bg_res[imgIndex++];
 	// alert(imgIndex);
 	/* judge the index of the picture to be set as background */
-	if (imgIndex >= res.length) {
+	if (imgIndex >= _bg_res.length) {
 		imgIndex = 0;
 	}
 	setBackground(bgImg);
 }
 
 /**
- * @description broadcast audio
+ * @description set background use picture
  * 
  * @param {String}
- *            res the picture to be set as background
+ *            _bg_res the picture to be set as background
  * @return {null} null
  */
-function setBackground(res) {
+function setBackground(_bg_res) {
 	/* Judge whether the img tag whose id is _bgImg exists or not */
 	var _bgImg = document.getElementById("_bgImg");
 	//alert(_bgImg);
-	if (_bgImg){
-		_bgImg.src = res;
-	} else{
+	if (_bgImg) {
+		_bgImg.src = _bg_res;
+	} else {
 		/* create div && set id\className && append to body */
 		var _bgDiv = document.createElement("div");
 		_bgDiv.id = "bgDiv";
@@ -275,13 +275,10 @@ function setBackground(res) {
 		/* create img && set src && append to div above */
 		var _bgImg = document.createElement("img");
 		_bgImg.id = "_bgImg";
-		_bgImg.src = res;
+		_bgImg.src = _bg_res;
 		var object = _bgDiv.appendChild(_bgImg);
 	}
 }
-
-
-
 
 /**
  * ########## ########## ########## ########## ########## ########## ########## ########## ########## *
@@ -295,13 +292,13 @@ function setBackground(res) {
  * @description control to set movie(video) as background whichi just is broadcast movie without controls
  * 
  * @param {int}
- *            mode 0 - diy resource(Array) 1 - use server's resource(String)
+ *            _movie_mode 0 - diy resource(Array) 1 - use server's resource(String)
  * @param {Array
- *            or Int} res Array - path of diy resource Int - broadcast mode, 0
+ *            or Int} _movie_res Array - path of diy resource Int - broadcast _movie_mode, 0
  *            for single replay, 1 for more replay
  * @return {null} null
  */
-function _movie(mode, res) {
+function _movie(_movie_mode, _movie_res) {
 	/* check what browser you are using */
 	var Sys = {};
 	var ua = navigator.userAgent.toLowerCase();
@@ -313,18 +310,18 @@ function _movie(mode, res) {
 			.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
 
 	/**
-	 * when mode equals 1, we should use server's audio else when mode equals 0,
+	 * when _movie_mode equals 1, we should use server's audio else when _movie_mode equals 0,
 	 * we should use diy audio
 	 */
-	if (1 == mode) {
+	if (1 == _movie_mode) {
 		/* video list on server */
 		var url = "http://121.199.46.162/work/pam/movie/";
 		var videoList = [ url + "movie", url + "xwqzzhxcp" ];
 
 		if (Sys.ie) {
-			// document.write('<bgsound src="' + res[0] + '.mp3" loop="-1">');
+			// document.write('<bgsound src="' + _movie_res[0] + '.mp3" loop="-1">');
 		} else {
-			if (1 == res) { // order play the movie(video) on server
+			if (1 == _movie_res) { // order play the movie(video) on server
 				broadcastSetList(videoList);
 			} else { // order play the audio on server
 				var slideIndex = parseInt(Math.random() * videoList.length);
@@ -338,7 +335,7 @@ function _movie(mode, res) {
 		 * tag
 		 */
 		if (Sys.ie) {
-		//if (Sys.chrome) {
+			//if (Sys.chrome) {
 			/* create div && append to body */
 			var _notice = document.createElement("div");
 			_notice.className = "ieMovieDiv";
@@ -347,13 +344,13 @@ function _movie(mode, res) {
 			/* create embed && set id\src\loop && append to div _notice */
 			var _embed = document.createElement("embed");
 			_embed.id = "_embed";
-			_embed.src = res[0] + ".avi";
+			_embed.src = _movie_res[0] + ".avi";
 			_embed.autostart = true;
 			_embed.loop = true;
 			_embed.className = "ieEmbed";
 			var object = _notice.appendChild(_embed);
 		} else {
-			broadcastSetList(res);
+			broadcastSetList(_movie_res);
 		}
 	}
 }
@@ -362,10 +359,10 @@ function _movie(mode, res) {
  * @description set movie(video) as background whichi just is broadcast movie without controls by list
  * 
  * @param {String}
- *            res the video list to be broadcast
+ *            _movie_res the video list to be broadcast
  * @return {null} null
  */
-function broadcastSetList(res) {
+function broadcastSetList(_movie_res) {
 	/* videoIndex is the index of resource array */
 	var videoIndex = 0;
 	/* create video */
@@ -380,22 +377,22 @@ function broadcastSetList(res) {
 		var _movieDiv = document.createElement("div");
 		_movieDiv.className = "movieDiv";
 		var object = document.body.appendChild(_movieDiv);
-		
+
 		/* append video to body */
 		var object = _movieDiv.appendChild(_video);
 
-		/* broadcat res[i] through video */
-		broadcastSet(_video, res[videoIndex++]);
+		/* broadcat _movie_res[i] through video */
+		broadcastSet(_video, _movie_res[videoIndex++]);
 
 		/* add the callback function addEventListener to video's object */
 		_video.addEventListener('ended', function() {
 			/* judge the index of the video to be broadcast */
-			if (videoIndex >= res.length) {
+			if (videoIndex >= _movie_res.length) {
 				videoIndex = 0;
 			}
 
-			/* broadcat res[i] through video */
-			broadcastSet(_video, res[videoIndex++]);
+			/* broadcat _movie_res[i] through video */
+			broadcastSet(_video, _movie_res[videoIndex++]);
 		}, false);
 	} else {
 		var _notice = document.createElement("div");
@@ -410,10 +407,10 @@ function broadcastSetList(res) {
  * @param {object}
  *            _movie the object create by document.createElement("movie")
  * @param {String}
- *            res the movie to be broadcast
+ *            _movie_res the movie to be broadcast
  * @return {null} null
  */
-function broadcastSet(_movie, res) {
+function broadcastSet(_movie, _movie_res) {
 	/* set id\controls\preload\autoplay\className */
 	_movie.id = "_movie";
 	//_movie.controls = "controls";
@@ -428,10 +425,10 @@ function broadcastSet(_movie, res) {
 	 * mp4 is much smaller than ogv, so make mp4 header
 	 */
 	if (_movie.canPlayType("video/mp4")) {
-		_movie.src = res + ".mp4";
+		_movie.src = _movie_res + ".mp4";
 	} else if (_movie.canPlayType("video/ogg")) {
-		_movie.src = res + ".ogv";
+		_movie.src = _movie_res + ".ogv";
 	} else if (_movie.canPlayType("video/webM")) {
-		_movie.src = res + ".webm";
+		_movie.src = _movie_res + ".webm";
 	}
 }
